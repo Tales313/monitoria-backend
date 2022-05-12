52aea50e36fded26f26a34309d6dd7c368fb6859
Tales313 <t4les313@gmail.com>
2022-05-11 22:05:17 -0300

    relacao entre inscricao e usuario

--- src/main/java/br/com/monitoria/web/controller/InscricaoController.java
+++ src/main/java/br/com/monitoria/web/controller/InscricaoController.java
@@ -1,6 +1,7 @@
 package br.com.monitoria.web.controller;
 
 import br.com.monitoria.domain.Inscricao;
+import br.com.monitoria.domain.Usuario;
 import br.com.monitoria.repository.InscricaoRepository;
 import br.com.monitoria.repository.VagaRepository;
 import br.com.monitoria.util.CalculaMedia;
@@ -8,6 +9,7 @@ import br.com.monitoria.web.request.InscricaoRequest;
 import br.com.monitoria.web.response.InscricaoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,9 +28,12 @@ public class InscricaoController {
     @ResponseStatus(HttpStatus.CREATED)
     public InscricaoResponse cadastrarInscricao(@Valid @RequestBody InscricaoRequest request) {
 
+        // pegando usuario logado
+        Usuario usuario = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
         Double media = CalculaMedia.calcular(request.getNotaDisciplina(), request.getCre());
 
-        Inscricao inscricao = request.toModel(vagaRepository, media);
+        Inscricao inscricao = request.toModel(vagaRepository, media, usuario);
         inscricao = inscricaoRepository.save(inscricao);
 
         return new InscricaoResponse(inscricao);
