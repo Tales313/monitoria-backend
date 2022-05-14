92db91a065d16e719455d1aec19877a006751fc9
Tales313 <t4les313@gmail.com>
2022-05-13 21:46:58 -0300

    adicionando excecoes no calculo da media e testes dessa classe

---
+++ src/main/java/br/com/monitoria/exception/CalculoMediaException.java
@@ -0,0 +1,7 @@
+package br.com.monitoria.exception;
+
+public class CalculoMediaException extends RuntimeException {
+    public CalculoMediaException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/br/com/monitoria/util/CalculaMediaTest.java
@@ -0,0 +1,48 @@
+package br.com.monitoria.util;
+
+import br.com.monitoria.exception.CalculoMediaException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculaMediaTest {
+
+    @Test
+    void sucessoEmCalcularMediaDeAluno() {
+        Double media = CalculaMedia.calcular(8.5, 7.0);
+        assertEquals(8.05, media);
+    }
+
+    @Test
+    void deveLancarExcecaoAoCalcularComNotaMenorQueZero() {
+        CalculoMediaException exception =
+                assertThrows(CalculoMediaException.class, () -> CalculaMedia.calcular(-0.01, 8.0));
+
+        assertEquals("A nota da disciplina deve estar entre 0 e 10", exception.getMessage());
+    }
+
+    @Test
+    void deveLancarExcecaoAoCalcularComNotaMaiorQueDez() {
+        CalculoMediaException exception =
+                assertThrows(CalculoMediaException.class, () -> CalculaMedia.calcular(10.01, 8.0));
+
+        assertEquals("A nota da disciplina deve estar entre 0 e 10", exception.getMessage());
+    }
+
+    @Test
+    void deveLancarExcecaoAoCalcularComCREMenorQueZero() {
+        CalculoMediaException exception =
+                assertThrows(CalculoMediaException.class, () -> CalculaMedia.calcular(8.0, -0.01));
+
+        assertEquals("O CRE deve estar entre 0 e 10", exception.getMessage());
+    }
+
+    @Test
+    void deveLancarExcecaoAoCalcularComCREMaiorQueDez() {
+        CalculoMediaException exception =
+                assertThrows(CalculoMediaException.class, () -> CalculaMedia.calcular(8.0, 10.01));
+
+        assertEquals("O CRE deve estar entre 0 e 10", exception.getMessage());
+    }
+
+}
