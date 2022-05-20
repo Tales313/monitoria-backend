54c37e7cd76359de5f1285c93b04e041fb9b710e
Tales313 <t4les313@gmail.com>
2022-05-19 21:42:11 -0300

    removendo senha do response de criacao de usuario

--- src/main/java/br/com/monitoria/web/response/UsuarioResponse.java
+++ src/main/java/br/com/monitoria/web/response/UsuarioResponse.java
@@ -6,25 +6,18 @@ public class UsuarioResponse {
 
     private String login;
 
-    private String senha;
-
     private LocalDateTime dataCadastro;
 
     private String matricula;
 
-    public UsuarioResponse(String login, String senha, LocalDateTime dataCadastro, String matricula) {
+    public UsuarioResponse(String login, LocalDateTime dataCadastro, String matricula) {
         this.login = login;
-        this.senha = senha;
         this.dataCadastro = dataCadastro;
         this.matricula = matricula;
     }
 
     public String getLogin() {
         return login;
-    }
-
-    public String getSenha() {
-        return senha;
     }
 
     public LocalDateTime getDataCadastro() {
