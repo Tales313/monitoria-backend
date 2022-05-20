1bae0e0d8f370589edb6d9896e302a6323942b01
Tales313 <t4les313@gmail.com>
2022-05-19 21:45:09 -0300

    removendo data cadastro do response de criacao de usuario

--- src/main/java/br/com/monitoria/web/response/UsuarioResponse.java
+++ src/main/java/br/com/monitoria/web/response/UsuarioResponse.java
@@ -6,22 +6,15 @@ public class UsuarioResponse {
 
     private String login;
 
-    private LocalDateTime dataCadastro;
-
     private String matricula;
 
-    public UsuarioResponse(String login, LocalDateTime dataCadastro, String matricula) {
+    public UsuarioResponse(String login, String matricula) {
         this.login = login;
-        this.dataCadastro = dataCadastro;
         this.matricula = matricula;
     }
 
     public String getLogin() {
         return login;
-    }
-
-    public LocalDateTime getDataCadastro() {
-        return dataCadastro;
     }
 
     public String getMatricula() {
