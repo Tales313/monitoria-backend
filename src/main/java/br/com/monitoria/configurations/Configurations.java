3abefe0f56995d115b40ac7b056d7c1a03ebbb0d
Tales313 <t4les313@gmail.com>
2022-09-02 10:48:10 -0300

    corrigindo erro de cors

--- src/main/java/br/com/monitoria/configurations/Configurations.java
+++ src/main/java/br/com/monitoria/configurations/Configurations.java
@@ -1,18 +1,43 @@
 package br.com.monitoria.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
 public class Configurations {
 
+    @Value("${allowed-origin.url}")
+    private String urlPermitida;
+
     @Bean
     public LocaleResolver localeResolver(){
         return new FixedLocaleResolver(new Locale("pt", "BR"));
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(urlPermitida));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 }
